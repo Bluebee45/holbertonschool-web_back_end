8ca47a2f1de83b78a335a29513b5cc0f3429a86c
Bluebee45 <santi72143@gmail.com>
2023-09-08 22:38:41 -0500

    add file

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """
-Deletion-resilient hypermedia pagination
+Description: The goal here is that if between two queries, certain rows are
+             removed from the dataset, the user does not miss items from
+             dataset when changing page
 """
 
 import csv
@@ -9,16 +11,18 @@ from typing import List, Dict
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
-
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        ''' Initialize instance. '''
         self.__dataset = None
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset"""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -28,56 +32,41 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0"""
+        """Dataset indexed by sorting position, starting at 0
+        """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
-            self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """
-        Hyper index method
-        Args:
-            index: current start index
-            page_size: number of element on a page
+        ''' Return dict of pagination data.
+            Dict key/value pairs consist of the following:
+              index - the start index of the page
+              next_index - the start index of the next page
+              page_size
+              page_size - the number of items on the page
+              data - the data in the page itself '''
+        assert 0 <= index < len(self.dataset())
 
-        Return:
-            Dictionary with the following elements
-            index: the current start index of the return page. That is the
-            index of the first item in the current page.
-            next_index: the next index to query with. That should be the index
-            of the first item after the last item on the current page.
-            page_size: the current page size
-            data: the actual page of the dataset
-        """
-        assert index < len(self.dataset())
-        assert index + page_size < len(self.dataset())
+        indexed_dataset = self.indexed_dataset()
+        indexed_page = {}
 
-        indexed_data = self.indexed_dataset()
-        keys = list(indexed_data.keys())
+        i = index
+        while (len(indexed_page) < page_size and i < len(self.dataset())):
+            if i in indexed_dataset:
+                indexed_page[i] = indexed_dataset[i]
+            i += 1
 
-        if index not in indexed_data:
-            start = keys[index]
-        else:
-            start = index
-
-        page = list()
-        for i in range(start, start + page_size):
-            if i not in indexed_data:
-                page.append(indexed_data[keys[i]])
-
-            else:
-                page.append(indexed_data[i])
-
-        next_index = index + page_size
-
-        if index not in keys:
-            next_index = keys[next_index]
+        page = list(indexed_page.values())
+        page_indices = indexed_page.keys()
 
         return {
-            "index": index,
-            "next_index": next_index,
-            "page_size": page_size,
-            "data": page,
+            'index': index,
+            'next_index': max(page_indices) + 1,
+            'page_size': len(page),
+            'data': page
         }
